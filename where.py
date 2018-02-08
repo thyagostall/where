2fe702a5b5e428c23b3ea7d7a3e70adf467a69c2
Thyago Stall <thstall@gmail.com>
2018-02-08 12:01:16 -0200

    Testing travis auto deploy

--- where.py
+++ where.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!", 200
+    return "Hello, world! (Second time)", 200
 
 # We only need this for local development.
 if __name__ == '__main__':
