6bd169610bc8e1f575424e9e3db62e714808af4e
thyagostall <thstall@gmail.com>
2018-02-08 21:03:04 -0200

    Introducing static files

--- where.py
+++ where.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def get_static(path):
+    return send_from_directory('static', path)
 
 # We only need this for local development.
 if __name__ == '__main__':
